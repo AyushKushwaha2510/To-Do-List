7527d7d697838ce5c9e42c1fc2aea89c062f0477
Ayush Kumar Kushwaha <ayush.kushwaha23138@gmail.com>
2025-05-29 17:45:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,4 +16,26 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
+    // const task = 
+    const addList = document.querySelector("#add-list");
+    addList.addEventListener("click", () => {
+        const input = document.getElementById("input-task").value;
+        if(input) {
+            const task = document.createElement("div");
+            task.innerHTML = `${input}`;
+            task.style.display = "flex";
+            task.style.position = "relative";
+            task.style.top = " 60px";
+            task.style.backgroundColor = "white";
+            task.style.height = "50px";
+            task.style.margin = "10px 0px";
+            task.style.width = "100%";
+            task.style.borderRadius = "5px";
+            task.style.boxShadow = "0px 3px 3px 0px rgba(164, 164, 164, 0.16)";
+               
+
+            dropBox.appendChild(task);
+        }
+    });
+
 });
